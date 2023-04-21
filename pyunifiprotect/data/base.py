13f3b6e6df4771a6ef04c861ed49a771b63fa5f3
J. Nick Koston <nick@koston.org>
2023-04-21 18:24:12 -0500

    avoid double dict construction

--- pyunifiprotect/data/base.py
+++ pyunifiprotect/data/base.py
@@ -500,12 +500,12 @@ class ProtectBaseObject(BaseModel):
         for key in data:
             setattr(self, key, convert_unifi_data(data[key], self.__fields__[key]))
 
-        # Calling dict with no params has a fast path which is MUCH faster
-        # so we pull out the excluded keys after
-        as_dict = self.dict()
-        for key in self.__class__._get_excluded_changed_fields():  # pylint: disable=protected-access
-            as_dict.pop(key, None)
-        self._initial_data = as_dict
+        excludes = self.__class__._get_excluded_changed_fields()
+        self._initial_data = {
+            key: value
+            for key, value in self.__dict__.items()
+            if key not in excludes
+        }
         return self
 
     def get_changed(self) -> Dict[str, Any]:
