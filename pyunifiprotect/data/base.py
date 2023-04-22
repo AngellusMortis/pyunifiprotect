06d8a1936ca5ab80f2e4a8bd3faf17d8d46813da
J. Nick Koston <nick@koston.org>
2023-04-21 19:01:56 -0500

    keep the one that matters

--- pyunifiprotect/data/base.py
+++ pyunifiprotect/data/base.py
@@ -502,7 +502,7 @@ class ProtectBaseObject(BaseModel):
             setattr(self, key, convert_unifi_data(data[key], self.__fields__[key]))
 
         excludes = self.__class__._get_excluded_changed_fields()
-        self._initial_data = self.dict(exclude=excludes)
+        self._initial_data = {k: v for k, v in self.__dict__.items() if k not in excludes}
         return self
 
     def get_changed(self) -> Dict[str, Any]:
