9f659f0041278da453600bf45579c15ad4e633a2
J. Nick Koston <nick@koston.org>
2023-04-29 08:28:38 -0500

    strip

--- pyunifiprotect/data/base.py
+++ pyunifiprotect/data/base.py
@@ -501,9 +501,8 @@ class ProtectBaseObject(BaseModel):
         for key in data:
             setattr(self, key, convert_unifi_data(data[key], self.__fields__[key]))
 
-        exclude_fields = self.__exclude_fields__ or {}
         excludes = self.__class__._get_excluded_changed_fields()  # pylint: disable=protected-access
-        self._initial_data = {k: v for k, v in self.__dict__.items() if k not in excludes and k not in exclude_fields}
+        self._initial_data = {k: v for k, v in self.dict().items() if k not in excludes}
         return self
 
     def get_changed(self) -> Dict[str, Any]:
