1c675a6e689a3b933e09c8bc33d11f56e1fb98cc
J. Nick Koston <nick@koston.org>
2023-06-21 11:44:34 +0200

    ruff --fix

--- pyunifiprotect/api.py
+++ pyunifiprotect/api.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 import asyncio
 from datetime import datetime, timedelta
 from http.cookies import Morsel
-from ipaddress import IPv4Address, IPv6Address
 import logging
 from pathlib import Path
 from tempfile import gettempdir
